a52115ad0b6c82136c5664ed11ef7391710c30e3
ManojSalugu <68062571+ManojSalugu@users.noreply.github.com>
2021-12-06 11:59:05 +0530

    Added authentication and permissions to profiles API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from profiles_api import models
+
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 from profiles_api import serializers
 
@@ -89,3 +93,10 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
